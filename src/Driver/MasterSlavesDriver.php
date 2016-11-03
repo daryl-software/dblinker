dbd5c78d4eacac84b8bacbcb4ac7d07dcdf53359
Mathieu Rochette <mathieu@easyflirt.com>
2016-11-03 14:15:19 +0100

    makes it possible to transform master slaves config and keep it cached

--- src/Driver/MasterSlavesDriver.php
+++ src/Driver/MasterSlavesDriver.php
@@ -19,13 +19,52 @@ trait MasterSlavesDriver
      * @return \Doctrine\DBAL\Driver\Connection The database connection.
      */
     public function connect(Array $params, $username = null, $password = null, Array $driverOptions = []) {
+        $cache = array_key_exists("config_cache", $driverOptions) ? $driverOptions["config_cache"] : null;
+
+        $configParams = array_intersect_key($params, array_flip(["master", "slaves"]));
+        $key = "dblinker.master-slave-config.".hash("sha256", serialize($configParams));
+
+        $config = null;
+        if ($cache !== null) {
+            assert($driverOptions["config_cache"] instanceof \Psr\Cache\CacheItemPoolInterface);
+            $cacheItem = $cache->getItem($key);
+            $config = $cacheItem->get();
+        }
+
+        if ($config === null) {
+            $config = $this->config($configParams, $driverOptions);
+        }
+
+        if ($cache !== null) {
+            $cacheItem->set($config);
+            $cache->save($cacheItem);
+        }
+
+        return new MasterSlavesConnection($config["master"], $config["slaves"]);
+    }
+
+    private function config(array $params, array $driverOptions)
+    {
         $driverKey = array_key_exists('driverClass', $params['master']) ? 'driverClass' : 'driver';
+
         $driverValue = $params['master'][$driverKey];
+
         $slaves = [];
         foreach ($params['slaves'] as $slaveParams) {
             $slaveParams[$driverKey] = $driverValue;
             $slaves[] = $slaveParams;
         }
-        return new MasterSlavesConnection($params['master'], $slaves);
+
+        $config = [
+            "master" => $params["master"],
+            "slaves" => $slaves,
+        ];
+
+        if (!array_key_exists("config_transform", $driverOptions)) {
+            return $config;
+        }
+
+        assert(is_callable($driverOptions["config_transform"]));
+        return $driverOptions["config_transform"]($config);
     }
 }
